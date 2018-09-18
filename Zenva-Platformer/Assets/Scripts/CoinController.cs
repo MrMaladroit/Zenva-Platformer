5919aa6c36d8456172330995767c09587ebe824a
Jim Beck <jamesbeckjr@gmail.com>
2018-09-17 22:17:04 -0400

    Prototype Level
    Added Coin and CoinController
    Created prototype level

---
+++ Zenva-Platformer/Assets/Scripts/CoinController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed = 100f;
+
+    private void Update()
+    {
+        float angleRotation = rotationSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * angleRotation, Space.World);
+    }
+}
