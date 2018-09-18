084b0277e23e0695043d2d7ecfd85b0ca059f8b6
Jim Beck <jamesbeckjr@gmail.com>
2018-09-18 10:03:09 -0400

    Enemy Movement

--- Zenva-Platformer/Assets/Scripts/EnemyController.cs
+++ Zenva-Platformer/Assets/Scripts/EnemyController.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private float enemySpeed = 3f;
+    [SerializeField]
+    private float rangeOfYMovemnt = 2f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int direction = 1;
+    private Vector3 initialPos;
+    
+
+    private void Start()
+    {
+        initialPos = transform.position;
+    }
+
+    private void Update()
+    {
+        float factor = direction == -1 ? 2f : 1;
+        float movementOnYAxis = factor * enemySpeed * Time.deltaTime * direction;
+
+        float newYPosition = transform.position.y + movementOnYAxis;
+
+        if(Mathf.Abs((newYPosition - initialPos.y)) > rangeOfYMovemnt)
+        {
+            direction *= -1;
+        }
+        else
+        {
+            transform.position += new Vector3(0, movementOnYAxis, 0);
+        }
+    }
 }
